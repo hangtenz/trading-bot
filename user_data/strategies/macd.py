33faaf78dd472bffaf4aa7b0a7ef6a262edef95f
hangtenz <ekalak.thang@gmail.com>
2022-06-13 08:40:40 +0700

    tune

--- user_data/strategies/macd.py
+++ user_data/strategies/macd.py
@@ -9,6 +9,10 @@ from unicodedata import decimal
 import numpy as np  # noqa
 import pandas as pd  # noqa
 from pandas import DataFrame
+import math
+import datetime
+from typing import Optional
+from freqtrade.persistence import Trade
 
 
 from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter, RealParameter,
@@ -32,7 +36,16 @@ class MACD(IStrategy):
     minimal_roi = {
         "0": 100
     }
+    position_adjustment_enable = True
 
+    distance_price = 0.02 #distance when calculate support,resistance
+    r2r = DecimalParameter(1.5,2.0,decimals=3,default=1.819,space="buy")
+    position_lost_percentage = DecimalParameter(0.5,1,decimals=3,default=0.935,space="buy")
+    risk_level = DecimalParameter(0.02,0.10,decimals=3,default=0.09,space="buy")
+    
+    n1 = 3 #count candle to find support,resistance
+    n2 = 3
+    
     stoploss = -1
     trailing_stop = False
     process_only_new_candles = False
@@ -40,8 +53,7 @@ class MACD(IStrategy):
     exit_profit_only = False
     ignore_roi_if_entry_signal = True
     startup_candle_count: int = 800
-
-    distance_price = 0.02 #distance when calculate support,resistance
+    percentage_stop = -1 # record when order, diff between open price and stoploss
 
     # Optional order type mapping.
     order_types = {
@@ -60,7 +72,10 @@ class MACD(IStrategy):
 
     plot_config = {
         'main_plot': {
-            'ema200_4h': {'color':'blue'},
+            f'ema200_{big_timeframe}': {'color':'blue'},
+            f'bb_upper_{big_timeframe}': {'color':'white'},
+            f'bb_middle_{big_timeframe}': {'color': 'white'},
+            f'bb_lower_{big_timeframe}': {'color': 'white'}
         },
         'subplots': {
             "MACD": {
@@ -69,14 +84,23 @@ class MACD(IStrategy):
         }
     }
 
-    def informative_pairs(self):
-        return []
+    # This is called when placing the initial order (opening trade)
+    def custom_stake_amount(self, pair: str, current_time: datetime, current_rate: float,
+                            proposed_stake: float, min_stake: Optional[float], max_stake: float,
+                            entry_tag: Optional[str], side: str, **kwargs) -> float:
+        # position size = account size x account risk / invalidation point
+        position_size = (max_stake*self.risk_level.value)/self.position_lost_percentage.value
+        if position_size < min_stake:
+            print(f"WARNING: enter position {pair} with risk level = {(min_stake*self.position_lost_percentage.value)/max_stake}")
+            return min_stake
+        return position_size
 
-    @informative('4h')
-    def populate_indicators_4h(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+
+    @informative(big_timeframe)
+    def populate_indicators_big_timeframe(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe['ema200'] = ta.EMA(dataframe['close'], timeperiod = 200)
-        n1 = 3
-        n2 = 3
+        n1 = self.n1
+        n2 = self.n2
         support = []
         resistance = []
 
@@ -106,14 +130,21 @@ class MACD(IStrategy):
         dataframe['touch_support'] = touch_support
         dataframe['touch_resistance'] = touch_resistance
 
-        dataframe = self.plot_support_resistance(dataframe)
+        bb_upper, bb_middle, bb_lower = ta.BBANDS(dataframe['close'], 20, 2.0, 2.0)
+        dataframe['bb_upper'] = bb_upper
+        dataframe['bb_middle'] = bb_middle
+        dataframe['bb_lower'] = bb_lower
+
+        #dataframe = self.plot_support_resistance(dataframe)
         return dataframe
 
     
     def leverage(self, pair: str, current_time: 'datetime', current_rate: float,
                  proposed_leverage: float, max_leverage: float, side: str,
                  **kwargs) -> float:
-        return 25
+        l = max(math.floor(self.position_lost_percentage.value/self.percentage_stop),1)
+        #print(f"percent_stop = {self.percentage_stop}, leverage={l}")
+        return l
 
 
     def distance_enough(self,p1,p2):
@@ -163,10 +194,10 @@ class MACD(IStrategy):
         for i in range(len(dataframe)):
             if dataframe.iloc[i]['is_support']:
                 dataframe[f'support_{i}'] = [dataframe.iloc[i]['low']] * len(dataframe)
-                main_plot[f'support_{i}_4h'] = {'color':'orange'}
+                main_plot[f'support_{i}_{self.big_timeframe}'] = {'color':'orange'}
             if dataframe.iloc[i]['is_resistance']:
                 dataframe[f'resistance_{i}'] = [dataframe.iloc[i]['high']] * len(dataframe)
-                main_plot[f'resistance_{i}_4h'] = {'color':'purple'}
+                main_plot[f'resistance_{i}_{self.big_timeframe}'] = {'color':'purple'}
         return dataframe
         
 
@@ -178,14 +209,14 @@ class MACD(IStrategy):
     
     def touch_support(self,dataframe:DataFrame,i:int,interesting_back_candle:int):
         for j in range(i-interesting_back_candle,i):
-            if dataframe.iloc[j]['touch_support_4h']:
-                return (True,dataframe.iloc[j]['low_4h'])
+            if dataframe.iloc[j][f'touch_support_{self.big_timeframe}']:
+                return (True,dataframe.iloc[j][f'low_{self.big_timeframe}'])
         return (False,None)
     
     def touch_resistance(self,dataframe:DataFrame,i:int,interesting_back_candle:int):
         for j in range(i-interesting_back_candle,i):
-            if dataframe.iloc[j]['touch_resistance_4h']:
-                return (True,dataframe.iloc[j]['high_4h'])
+            if dataframe.iloc[j][f'touch_resistance_{self.big_timeframe}']:
+                return (True,dataframe.iloc[j][f'high_{self.big_timeframe}'])
         return (False,None)
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
@@ -205,11 +236,13 @@ class MACD(IStrategy):
             touch_support,s = self.touch_support(dataframe,i,interesting_back_candle)
             current = dataframe.iloc[i]
             #enter long
-            if current['close'] > current['ema200_4h'] and current['macd_hist']>0 and dataframe.iloc[i-1]['macd_hist']<0 and touch_support:
+            if entering==None and current['close'] > current[f'ema200_{self.big_timeframe}'] and current['low'] > current[f'ema200_{self.big_timeframe}'] and current['macd_hist']>0 and dataframe.iloc[i-1]['macd_hist']<0 and touch_support:
                 enter_long[i] = True
                 entering = 'long'
                 price_stop_loss = s - (s*distance_stop_loss)
-                exit_price = (current['close'] - s)*(1.5+distance_stop_loss) + current['close']
+                self.percentage_stop = (current['close'] - price_stop_loss) / price_stop_loss
+                exit_price = (current['close'] - price_stop_loss)*self.r2r.value + current['close']
+                #print(f"enter long position {current['close']} with exit_price={exit_price} stoploss = {price_stop_loss} percentage stop = {self.percentage_stop}")
             #take profit long
             if entering=='long' and current['close'] >= exit_price:
                 entering = None
@@ -221,11 +254,14 @@ class MACD(IStrategy):
             
             touch_resistance,r = self.touch_resistance(dataframe,i,interesting_back_candle)
             #enter short
-            if current['close'] < current['ema200_4h'] and current['macd_hist']<0 and dataframe.iloc[i-1]['macd_hist']>0 and touch_resistance:
+            # if touch_resistance:
+            #     print(f"date={current['date']} {entering} {current['close']} {current[f'ema200_{self.big_timeframe}']} {current['high']} {current[f'ema200_{self.big_timeframe}']} {current['macd_hist']}  {dataframe.iloc[i-1]['macd_hist']} ")
+            if entering==None and current['close'] < current[f'ema200_{self.big_timeframe}'] and current['high'] < current[f'ema200_{self.big_timeframe}'] and current['macd_hist']<0 and dataframe.iloc[i-1]['macd_hist']>0 and touch_resistance:
                 enter_short[i] = True
                 entering = 'short'
                 price_stop_loss = r + (r*distance_stop_loss)
-                exit_price = current['close'] - (r-current['close'])*(1.5+distance_stop_loss)
+                self.percentage_stop = (price_stop_loss - current['close']) / current['close']
+                exit_price = current['close'] - (price_stop_loss-current['close'])*self.r2r.value
             #take profit short
             if entering=='short' and current['close']<=exit_price:
                 entering = None
